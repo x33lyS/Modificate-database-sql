2d8fa51fc7e78857b5492361f108b5c1cda257f8
FLEJOSNE <florian.lejosne@ynov.com>
2021-12-14 10:20:47 +0100

    adding some file

--- new-db.sql
+++ new-db.sql
@@ -1,33 +1,26 @@
--- Game Table
-ALTER table Game
-    RENAME COLUMN Name To ExName;
-ALTER table Game
-    ADD Name VARCHAR2(30);
-UPDATE Game SET Name = ExName;
-ALTER TABLE game
-DROP "ExName";
-
 --Place Table
 CREATE TABLE Place
 (
-    idPlace integer  not null
+    idPlace integer not null
         primary key autoincrement,
-    Name         VARCHAR2(30),
-    Address      VARCHAR2(30),
-    City         VARCHAR2(30)
+    Name    VARCHAR2(30) not null,
+    Address VARCHAR2(30) not null,
+    City    VARCHAR2(30) not null
 );
 INSERT INTO Place (Name, Address, City)
-SELECT placeName, address, City FROM tournament;
+SELECT placeName, address, City
+FROM tournament;
 
 --Employee-Data
 CREATE TABLE Employee_Data
-( idEmployee integer  not null
-      primary key autoincrement,
-  Lastname VARCHAR2(30) ,
-  FirstName VARCHAR2(30),
-  Gender VARCHAR2(30),
-  Age INTEGER,
-  Wage INTEGER
+(
+    idEmployee integer not null
+        primary key autoincrement,
+    Lastname   VARCHAR2(30) not null,
+    FirstName  VARCHAR2(30) not null,
+    Gender     VARCHAR2(30) not null,
+    Age        INTEGER not null,
+    Wage       INTEGER not null
 );
 
 INSERT INTO Employee_Data
@@ -36,159 +29,168 @@ INSERT INTO Employee_Data
  Gender,
  Age,
  Wage)
-SELECT
-    Lastname,
-    Firstname,
-    Gender,
-    Age,
-    Wage
-FROM
-    Staff
+SELECT Lastname,
+       Firstname,
+       Gender,
+       Age,
+       Wage
+FROM Staff
 UNION ALL
-SELECT
-    Lastname,
-    Firstname,
-    Gender,
-    Age,
-    Wage
-FROM
-    PLayer
+SELECT Lastname,
+       Firstname,
+       Gender,
+       Age,
+       Wage
+FROM PLayer
 UNION ALL
-SELECT
-    Lastname,
-    Firstname,
-    Gender,
-    Age,
-    Wage
-FROM
-    Coach
+SELECT Lastname,
+       Firstname,
+       Gender,
+       Age,
+       Wage
+FROM Coach
 ;
 
 
 --Staff Table
 create table newstaff
 (
-    idStaff   integer  not null
+    idStaff    integer not null
         primary key autoincrement,
-    lastname  text(30) not null,
-    firstname text(30) not null,
-    gender    text(30) not null,
-    age       integer  not null,
-    wage      integer  not NULL,
+    lastname   text(30) not null,
+    firstname  text(30) not null,
+    gender     text(30) not null,
+    age        integer not null,
+    wage       integer not NULL,
     idEmployee INTEGER not null
 );
-INSERT INTO newstaff("idEmployee",lastname,firstname,gender,age,wage)
-SELECT  Employee_Data.IdEmployee, Employee_Data.Lastname , Employee_Data.FirstName , Employee_Data.Gender , Employee_Data.Age ,Employee_Data.wage
+INSERT INTO newstaff("idEmployee", lastname, firstname, gender, age, wage)
+SELECT Employee_Data.IdEmployee, Employee_Data.Lastname, Employee_Data.FirstName, Employee_Data.Gender, Employee_Data.Age, Employee_Data.wage
 FROM Employee_Data
-         INNER JOIN staff ON Employee_Data.Lastname = staff.lastname;
+         INNER JOIN staff ON Employee_Data.Lastname = staff.lastname AND Employee_Data.FirstName = staff.FirstName;
 DROP TABLE staff;
 ALTER TABLE newstaff
     RENAME TO staff;
 ALTER TABLE staff
-DROP lastname;
+DROP
+lastname;
 ALTER TABLE staff
-DROP age;
+DROP
+age;
 ALTER TABLE staff
-DROP wage;
+DROP
+wage;
 ALTER TABLE staff
-DROP gender;
+DROP
+gender;
 ALTER TABLE staff
-DROP firstname;
+DROP
+firstname;
 
 -- Player Table
 create table newplayer
 (
-    idPlayer   integer  not null
+    idPlayer   integer not null
         primary key autoincrement,
-    lastname  VARCHAR2(30) not null,
-    firstname VARCHAR2(30) not null,
-    gender    VARCVARCHAR2(30) not null,
-    age       integer  not null,
-    wage      integer  not NULL,
-    IdGame      integer
-        constraint FK_9d24c46ddfcc57ed29c453c2b50
-            references game,
-    ranking   INTEGER NOT NULL,
-    idEmployee INTEGER not null
+    lastname   VARCHAR2(30) not null,
+    firstname  VARCHAR2(30) not null,
+    gender     VARCVARCHAR2(30) not null,
+    age        integer not null,
+    wage       integer not NULL,
+    IdGame     integer,
+    ranking    INTEGER NOT NULL,
+    idEmployee INTEGER not null,
+    FOREIGN KEY (IdGame) REFERENCES game (IdGame)
 );
-INSERT INTO newplayer("idEmployee",lastname,firstname,gender,age,wage,ranking)
-SELECT  Employee_Data.IdEmployee, Employee_Data.Lastname , Employee_Data.FirstName , Employee_Data.Gender , Employee_Data.Age ,Employee_Data.wage,player.ranking
+INSERT INTO newplayer("idEmployee", lastname, firstname, gender, age, wage, ranking)
+SELECT Employee_Data.IdEmployee, Employee_Data.Lastname, Employee_Data.FirstName, Employee_Data.Gender, Employee_Data.Age, Employee_Data.wage, player.ranking
 FROM Employee_Data
-         INNER JOIN player ON Employee_Data.Lastname = player.lastname;
+         INNER JOIN player ON Employee_Data.Lastname = player.lastname AND Employee_Data.FirstName = player.FirstName;
 DROP TABLE player;
 ALTER TABLE newplayer
     RENAME TO player;
 ALTER TABLE player
-DROP lastname;
+DROP
+lastname;
 ALTER TABLE player
-DROP age;
+DROP
+age;
 ALTER TABLE player
-DROP wage;
+DROP
+wage;
 ALTER TABLE player
-DROP gender;
+DROP
+gender;
 ALTER TABLE player
-DROP firstname;
+DROP
+firstname;
 
 -- Coach Table
 create table newcoach
 (
-    idCoach   integer  not null
+    idCoach     integer not null
         primary key autoincrement,
-    lastname  VARCHAR2(30) not null,
-    firstname VARCHAR2(30) not null,
-    gender    VARCHAR2(30) not null,
-    age       integer  not null,
-    wage      integer  not NULL,
-    IdGame      integer
-        constraint FK_8ce1eeb16389d742df20c458324
-            references game,
-    licenseDate   VARCHAR2(30) NOT NULL,
-    idEmployee INTEGER not null
+    lastname    VARCHAR2(30) not null,
+    firstname   VARCHAR2(30) not null,
+    gender      VARCHAR2(30) not null,
+    age         integer not null,
+    wage        integer not NULL,
+    IdGame      integer,
+    licenseDate VARCHAR2(30) NOT NULL,
+    idEmployee  INTEGER not null,
+    FOREIGN KEY (IdGame) REFERENCES game (IdGame)
 );
-INSERT INTO newcoach("idEmployee",lastname,firstname,gender,age,wage,licenseDate)
-SELECT  Employee_Data.IdEmployee, Employee_Data.Lastname , Employee_Data.FirstName , Employee_Data.Gender , Employee_Data.Age ,Employee_Data.wage, coach.licenseDate
+INSERT INTO newcoach("idEmployee", lastname, firstname, gender, age, wage, licenseDate)
+SELECT Employee_Data.IdEmployee, Employee_Data.Lastname, Employee_Data.FirstName, Employee_Data.Gender, Employee_Data.Age, Employee_Data.wage, coach.licenseDate
 FROM Employee_Data
-         INNER JOIN coach ON Employee_Data.Lastname = coach.lastname;
+         INNER JOIN coach ON Employee_Data.Lastname = coach.lastname AND Employee_Data.FirstName = coach.FirstName;
 DROP TABLE coach;
 ALTER TABLE newcoach
     RENAME TO coach;
 ALTER TABLE coach
-DROP lastname;
+DROP
+lastname;
 ALTER TABLE coach
-DROP age;
+DROP
+age;
 ALTER TABLE coach
-DROP wage;
+DROP
+wage;
 ALTER TABLE coach
-DROP gender;
+DROP
+gender;
 ALTER TABLE coach
-DROP firstname;
+DROP
+firstname;
 
 --Tournament Table
 create table newtournament
 (
-    idTournament   integer  not null
+    idTournament integer not null
         primary key autoincrement,
-    IdPlace INTEGER NOT NULL,
-    IdGame      integer
-        constraint FK_a7cdb11d62c6d19742ad1a8657c
-            references game,
-    Date VARCHAR2(30) NOT NULL,
-    Duration INTEGER,
-    PlaceName VARCHAR2(30),
-    Address VARCHAR2(30),
-    City VARCHAR2(30)
+    IdPlace      INTEGER NOT NULL,
+    IdGame       integer,
+    Date         VARCHAR2(30) NOT NULL,
+    Duration     INTEGER not null,
+    PlaceName    VARCHAR2(30) not null,
+    Address      VARCHAR2(30) not null,
+    City         VARCHAR2(30) not null,
+    FOREIGN KEY (IdGame) REFERENCES game (IdGame)
 
 );
-INSERT INTO newtournament(idPlace,Date,Duration,PlaceName,Address,City)
-SELECT  Place.idPlace, tournament.date, tournament.duration, tournament.placeName, tournament.address, tournament.city
+INSERT INTO newtournament(idPlace, Date, Duration, PlaceName, Address, City)
+SELECT Place.idPlace, tournament.date, tournament.duration, tournament.placeName, tournament.address, tournament.city
 FROM tournament
-         INNER JOIN place ON tournament.placeName = place.name;
+         INNER JOIN place ON tournament.placeName = place.name AND tournament.address = place.address;
 DROP TABLE tournament;
 ALTER TABLE newtournament
     RENAME TO tournament;
 ALTER TABLE tournament
-DROP city;
+DROP
+city;
 ALTER TABLE tournament
-DROP address;
+DROP
+address;
 ALTER TABLE tournament
-DROP placeName;
+DROP
+placeName;
